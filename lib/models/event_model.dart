557a200d6128c8cd36ca607c2e7e871fab991681
Pratik Temkar <pratiktemkarofficial@gmail.com>
2019-11-11 09:18:50 +0530

    Events added.

--- lib/models/event_model.dart
+++ lib/models/event_model.dart
@@ -1,10 +1,12 @@
 class Event {
+  String name;
   String imageUrl;
   String city;
   String country;
   String description;
 
   Event({
+    this.name,
     this.imageUrl,
     this.city,
     this.country,
@@ -14,22 +16,44 @@ class Event {
 
 List<Event> events = [
   Event(
+      name: 'Event 0',
       imageUrl: 'assets/images/0.jpg',
       city: 'Pune',
       country: 'India',
       description: 'Very good event.'),
   Event(
+      name: 'Event 1',
       imageUrl: 'assets/images/1.jpg',
       city: 'Delhi',
       country: 'India',
       description: 'Very good event.'),
   Event(
+      name: 'Event 2',
       imageUrl: 'assets/images/2.jpg',
       city: 'Hyderabad',
       country: 'India',
       description: 'Very good event.'),
   Event(
+      name: 'Event 3',
       imageUrl: 'assets/images/3.jpg',
+      city: 'Bangalore',
+      country: 'India',
+      description: 'Very good event.'),
+  Event(
+      name: 'Event 4',
+      imageUrl: 'assets/images/4.jpg',
+      city: 'Bangalore',
+      country: 'India',
+      description: 'Very good event.'),
+  Event(
+      name: 'Event 5',
+      imageUrl: 'assets/images/5.jpg',
+      city: 'Bangalore',
+      country: 'India',
+      description: 'Very good event.'),
+  Event(
+      name: 'Event 6',
+      imageUrl: 'assets/images/6.jpg',
       city: 'Bangalore',
       country: 'India',
       description: 'Very good event.'),
